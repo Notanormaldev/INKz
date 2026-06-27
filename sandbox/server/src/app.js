f438b11c04604e777e5b5f666149cdcec1e325e2
Notanormaldev <harshpatel20050@gmail.com>
2026-06-27 10:08:31 +0530

    feat: initialize sandbox server with Express API and Kubernetes deployment configuration

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -1,11 +1,18 @@
-import express from 'express'
+import express, { urlencoded } from 'express'
 import morgan from "morgan"
 
 
 const app = express()
 
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: true }))
 
-
-
+app.get('/api/sandbox/health', (req, res) => {
+    return res.status(200).json({
+        message: "Sandbox api is healthy",
+        status:'ok'
+    })
+})
 
 export default app
