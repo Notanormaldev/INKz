a7781b876df9bb7ebfcd0accfc27b83ceb9f2e89
Notanormaldev <harshpatel20050@gmail.com>
2026-07-16 10:18:16 +0530

    feat: initialize authentication service with Express, MongoDB, and Google OAuth2 strategy

--- auth/src/app.js
+++ auth/src/app.js
@@ -11,6 +11,19 @@ import cookies from 'cookie-parser'
 
 const app =express()
 
+app.use(express.json())
+app.use(cookies())
+app.use(morgan("dev"))
+
+passport.use(new GoogleStrategy({
+    clientID:process.env.GOOGLE_CLIENT_ID,
+    clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL:"/api/auth/google/callback",
+},(accesstoken,refreshtoken,profile,done)=>{
+    // console.log(profile);
+    return  done(null,profile)
+
+}))
 
 export default app 
 
