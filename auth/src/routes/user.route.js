3424d2c83e751e3cd84ff1ce106b41b6185d2180
Notanormaldev <harshpatel20050@gmail.com>
2026-07-16 10:55:02 +0530

    feat: implement user model and Google OAuth authentication routes

---
+++ auth/src/routes/user.route.js
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import User from "../models/user.model.js";
+import passport from "passport";
+
+
+const router = Router()
+
+router.get('/google',passport.authenticate('google',{scope:['profile','email']}))
+
+router.get('/google/callback',passport.authenticate('google',{failureRedirect:"/"}),async (req,res) => {
+    try {
+
+        const {id,displayName,emails,photos}=req.user;
+        let user = await User.findOne({googleId:id})
+
+        if(!user){
+            user = await User.create({
+                googleId:id,
+                name:displayName,
+                email:emails[0].value,
+                profile_pic:photos[0].value
+            })
+            await user.save();
+        }
+
+        const token = jwt.sign({id:user._id},process.env.JWT,{expiresIn:"1w"})
+        res.cookie("token",token,{httpOnly:true})
+        res.redirect("/")
+        
+    } catch (error) {
+        console.log(error);
+        res.redirect("/login")
+    }
+})
+
+
+export default router;
